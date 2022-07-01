81bba31b28dc9eedf6fdebae8338568172012d37
mgorem <oremgitonga22@gmail.com>
2022-07-01 14:55:30 +0300

    learn about methods

---
+++ src/chapter5/Greetings.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+import java.util.Scanner;
+
+/*
+* METHODS
+* Write a method that asks a user for their name, then greets them by name.
+ */
+
+// Main method is used to control the flow of the program.
+public class Greetings {
+    public static void main(String args[]){
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+
+        // Call the greetUser Method
+        greetUser(userName);
+    }
+
+    // Greet User Method (returns nothing, but when triggered, greets user).
+    public static void greetUser(String name){
+        System.out.println("Hi there, " + name);
+    }
+}
